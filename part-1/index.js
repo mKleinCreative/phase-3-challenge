89cebdeaa311b6ecc2e9ac24861ffe880d323f2f
Michael Klein <miketheklein@gmail.com>
2017-07-24 14:04:50 -0700

    Routes are completing math, now for part 2

--- part-1/index.js
+++ part-1/index.js
@@ -32,7 +32,7 @@ app.get( '/api/supported-operations', (request, response, next) => {
 // response: {"result": 110.25}
 // request content type: application/json
 
-app.get( '/api/square', function (request, response, next) {
+app.get( '/api/square', (request, response, next) => {
   const req = request.query
   const result = Math.pow( req.number, 2 )
   console.log( '---===number===---', result); 
@@ -43,6 +43,61 @@ app.get( '/api/square', function (request, response, next) {
   } else {
     response.json({
       message: "No number provided."
+    })
+  }
+})
+
+/* Third route
+  request: POST /api/compute
+  request body params: {"operator": "+",
+                        "operands": [3,4]}
+  request content type: application/json
+  response: {"result": 7}
+  
+  
+  request: POST /api/compute
+  request body params: {"operator": "/",
+                        "operands": [5,3]}
+  response: {"result": 1.67}
+  request content type: application/json
+  
+  
+  request: POST /api/compute
+  request body params: {"operator": "?",
+                        "operands": [5,3]}
+  request content type: application/json
+  response: {"error": "invalid operator ?. Valid operators are /, +, -, *"}
+  response content type: application/json
+  response status code: 404 */
+
+app.post( '/api/compute', (request, response, next) => {
+  console.log( '---===request.body===---', request.body ); 
+  const { operator, operands } = request.body
+  console.log( '---===operands===---', operands ); 
+  console.log( '---===operator===---', operator ); 
+  let num1 = parseInt(operands[0])
+  let num2 = parseInt(operands[1])
+  let finalResult = ""
+  if ( operator !== "+" || "-" || "*" || "/") {
+    response.json({
+      error: `invalid operator ${operator}. Valid operators are /, +, -, *`
+    })
+  } else {
+      if ( operator === "+") {
+        finalResult = num1 + num2
+      }
+      if ( operator === "-") {
+        finalResult = num1 - num2
+      }
+      if ( operator === "*") {
+        finalResult = num1 * num2
+      }
+      if ( operator === "/") {
+        finalResult = num1 / num2
+      }
+
+    response.json({
+      Result: finalResult.toFixed(2)
     })
   }
 })
